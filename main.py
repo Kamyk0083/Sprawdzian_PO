be7afec4971438c46aa66e9f1dffe77dbcb880bc
Kamyk0083 <kaminski.ksawery00@gmail.com>
2023-10-12 13:31:32 +0200

    Sprawdzian PO
    testy przechodzą i jak na razie wszystko jest dobrze

---
+++ main.py
@@ -0,0 +1,30 @@
+import math
+
+def czy_pierwsza(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def liczby_pierwsze_do_n(n):
+    if n <= 1:
+        return []
+    liczby_pierwsze = []
+    for i in range(2, n + 1):
+        if czy_pierwsza(i):
+            liczby_pierwsze.append(i)
+    return liczby_pierwsze
+
+if __name__ == '__main__':
+    n = int(input("Podaj liczbę całkowitą: "))
+    liczby_pierwsze = liczby_pierwsze_do_n(n)
+    for liczba in liczby_pierwsze:
+        print(liczba)
---
+++ main_test.py
@@ -0,0 +1,98 @@
+import pytest
+from main import czy_pierwsza, liczby_pierwsze_do_n
+
+
+def test_czy_pierwsza_dla_liczby_2():
+    assert czy_pierwsza(2) == True
+
+
+def test_czy_pierwsza_dla_liczby_3():
+    assert czy_pierwsza(3) == True
+
+
+def test_czy_pierwsza_dla_liczby_4():
+    assert czy_pierwsza(4) == False
+
+
+def test_czy_pierwsza_dla_liczby_17():
+    assert czy_pierwsza(17) == True
+
+
+def test_czy_pierwsza_dla_liczby_25():
+    assert czy_pierwsza(25) == False
+
+
+def test_czy_pierwsza_dla_liczby_ujemnej():
+    assert czy_pierwsza(-5) == False
+
+
+def test_czy_pierwsza_dla_liczby_0():
+    assert czy_pierwsza(0) == False
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_10():
+    result = liczby_pierwsze_do_n(10)
+    assert result == [2, 3, 5, 7]
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_20():
+    result = liczby_pierwsze_do_n(20)
+    assert result == [2, 3, 5, 7, 11, 13, 17, 19]
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_1():
+    result = liczby_pierwsze_do_n(1)
+    assert result == []
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_2():
+    result = liczby_pierwsze_do_n(2)
+    assert result == [2]
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_ujemnej():
+    result = liczby_pierwsze_do_n(-5)
+    assert result == []
+
+
+def test_czy_pierwsza_dla_liczby_97():
+    assert czy_pierwsza(97) == True
+
+
+def test_czy_pierwsza_dla_liczby_100():
+    assert czy_pierwsza(100) == False
+
+
+def test_czy_pierwsza_dla_97_razy_97():
+    assert czy_pierwsza(97 * 97) == False
+
+
+def test_liczby_pierwsze_do_n_dla_n_rowne_100():
+    result = liczby_pierwsze_do_n(100)
+    assert result == [
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47,
+        53,
+        59,
+        61,
+        67,
+        71,
+        73,
+        79,
+        83,
+        89,
+        97,
+    ]
